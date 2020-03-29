79a8ef10cba54d0754793b431ae2f53986796bc9
Brian Rentsch <brentsch882@gmail.com>
2020-03-29 16:45:15 -0400

    Add functionality to parse page number and offset from logical addresses

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define TLB_SIZE 16
+/* Sizes in terms of bits */
+#define ADDR_SIZE 32
+#define PAGE_NUM_SIZE 8
+#define OFFSET_SIZE 8
 
+/* Sizes in terms of entries */
+#define TLB_SIZE 16    
+
+/* Structure for a single entry in the TLB */
 typedef struct _TLB_ENTRY
 {
     int page_num;
@@ -12,11 +20,14 @@ typedef struct _TLB_ENTRY
 
 int main(int argc, char** argv)
 {
-	char filename[] = "addresses.txt";
+	char filename[] = "addresses.txt"; // File to read logical addresses from
 	FILE *fp;
-	char str[5];
-	int address;
-
+	char str[5];  // Buffer to hold a single address read from file
+	uint32_t address;  // Integer value of current address read from file (32 bits)    
+    
+    uint8_t page_num; // Page number parsed from logical address (bits 15 - 8)
+    uint8_t offset;   // Offset within the frame indicated by page_num parsed from logical address (bits 7 - 0)
+    
     TLB_ENTRY *tlb = (TLB_ENTRY*)malloc(sizeof(TLB_ENTRY) * TLB_SIZE);
     
 	/* Open file for reading */
@@ -31,8 +42,13 @@ int main(int argc, char** argv)
 		/* Convert address to int type */
 		address = atoi(str);
 		if (address == 0) {	// ignore newline characters
-			continue;
-		}	
+            continue;
+		}
+
+        page_num = (address << (ADDR_SIZE - (PAGE_NUM_SIZE + OFFSET_SIZE))) >> (ADDR_SIZE - PAGE_NUM_SIZE);
+        offset = (address << (ADDR_SIZE - OFFSET_SIZE)) >> (ADDR_SIZE - OFFSET_SIZE);
+
+        printf("address: %x\npage_num: %x\noffset: %x\n", address, page_num, offset); // I used this for testing -- feel free to delete whenever
 	}
 
 	/* Close file after all entries are read */
