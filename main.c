ec0ef3300da5fe174352ba9b5d4df2388eed1031
Brian Rentsch <brentsch882@gmail.com>
2020-03-29 17:10:12 -0400

    Add page table structure and comments outlining next steps

--- main.c
+++ main.c
@@ -8,8 +8,11 @@
 #define PAGE_NUM_SIZE 8
 #define OFFSET_SIZE 8
 
-/* Sizes in terms of entries */
-#define TLB_SIZE 16    
+#define TLB_SIZE 16 // TLB has 16 entries
+#define PT_SIZE 256
+#define NUM_FRAMES 256
+
+int page_table[PT_SIZE]; //Global page table
 
 /* Structure for a single entry in the TLB */
 typedef struct _TLB_ENTRY
@@ -17,6 +20,11 @@ typedef struct _TLB_ENTRY
     int page_num;
     int frame_num;    
 } TLB_ENTRY;    
+
+int search_pt(int page_num)
+{
+    return page_table[page_num];
+}
 
 int main(int argc, char** argv)
 {
@@ -29,6 +37,10 @@ int main(int argc, char** argv)
     uint8_t offset;   // Offset within the frame indicated by page_num parsed from logical address (bits 7 - 0)
     
     TLB_ENTRY *tlb = (TLB_ENTRY*)malloc(sizeof(TLB_ENTRY) * TLB_SIZE);
+
+    /* Initialize page table values to -1 (indicating empty)*/
+    for(int i = 0; i < PT_SIZE; i++) 
+        page_table[i] = -1;
     
     /* Open file for reading */
     fp = fopen(filename, "r");
@@ -49,10 +61,25 @@ int main(int argc, char** argv)
         offset = (address << (ADDR_SIZE - OFFSET_SIZE)) >> (ADDR_SIZE - OFFSET_SIZE);
 
         printf("address: %x\npage_num: %x\noffset: %x\n", address, page_num, offset); // I used this for testing -- feel free to delete whenever
-    }
 
+        /* TODO: check TLB for page_num. 
+
+            TLB HIT: get the corresponding frame_num and access memory at that frame combined with the parsed offset.
+           
+            TLB MISS: Check page table.
+
+                PT HIT: get the corresponding frame_num and access memory at that frame + offset. Update the TLB with page_num and frame_num.
+                
+                PT MISS: Handle page fault. Update PT and TLB.
+        */            
+        
+    }
+    
     /* Close file after all entries are read */
     fclose(fp);
 
+    /* Free TLB memory */
+    free(tlb);
+    
     return(0);
 }
