b20af560a15f3ec1132f3469847d9b248338edf0
Brian Rentsch <brentsch882@gmail.com>
2020-03-29 16:46:38 -0400

    Replace tabs with spaces

--- main.c
+++ main.c
@@ -20,39 +20,39 @@ typedef struct _TLB_ENTRY
 
 int main(int argc, char** argv)
 {
-	char filename[] = "addresses.txt"; // File to read logical addresses from
-	FILE *fp;
-	char str[5];  // Buffer to hold a single address read from file
-	uint32_t address;  // Integer value of current address read from file (32 bits)    
+    char filename[] = "addresses.txt"; // File to read logical addresses from
+    FILE *fp;
+    char str[5];  // Buffer to hold a single address read from file
+    uint32_t address;  // Integer value of current address read from file (32 bits)    
     
     uint8_t page_num; // Page number parsed from logical address (bits 15 - 8)
     uint8_t offset;   // Offset within the frame indicated by page_num parsed from logical address (bits 7 - 0)
     
     TLB_ENTRY *tlb = (TLB_ENTRY*)malloc(sizeof(TLB_ENTRY) * TLB_SIZE);
     
-	/* Open file for reading */
-	fp = fopen(filename, "r");
-	if (fp == NULL) {
-		perror("Error opening file");
-		return(-1);
-	}
+    /* Open file for reading */
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        perror("Error opening file");
+        return(-1);
+    }
 
-	/* Read address file line by line */
-	while (fgets(str, 6, fp) != NULL) {
-		/* Convert address to int type */
-		address = atoi(str);
-		if (address == 0) {	// ignore newline characters
+    /* Read address file line by line */
+    while (fgets(str, 6, fp) != NULL) {
+        /* Convert address to int type */
+        address = atoi(str);
+        if (address == 0) {    // ignore newline characters
             continue;
-		}
+        }
 
         page_num = (address << (ADDR_SIZE - (PAGE_NUM_SIZE + OFFSET_SIZE))) >> (ADDR_SIZE - PAGE_NUM_SIZE);
         offset = (address << (ADDR_SIZE - OFFSET_SIZE)) >> (ADDR_SIZE - OFFSET_SIZE);
 
         printf("address: %x\npage_num: %x\noffset: %x\n", address, page_num, offset); // I used this for testing -- feel free to delete whenever
-	}
+    }
 
-	/* Close file after all entries are read */
-	fclose(fp);
+    /* Close file after all entries are read */
+    fclose(fp);
 
-	return(0);
+    return(0);
 }
