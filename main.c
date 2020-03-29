76a98ad9a8ec48031ceab042f4b4ca55910ef50d
Dan Funke <funked1@tcnj.edu>
2020-03-29 15:16:10 -0400

    converting addresses from txt file to integers

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	char filename[] = "addresses.txt";
 	FILE *fp;
 	char str[5];
-
+	int address;
 
 	/* Open file for reading */
 	fp = fopen(filename, "r");
@@ -19,8 +19,14 @@ int main(int argc, char** argv)
 
 	/* Read address file line by line */
 	while (fgets(str, 6, fp) != NULL) {
-		puts(str);
+		/* Convert address to int type */
+		address = atoi(str);
+		if (address == 0) {	// ignore newline characters
+			continue;
+		}	
 	}
+
+	/* Close file after all entries are read */
 	fclose(fp);
 
 	return(0);
